89416513fbf4090da187a84c3a7f4b24cd051053
Casey McGrath <mcgrathc@hawaii.edu>
2020-11-12 12:42:50 -1000

    add dictionary approach

--- FIPS_data.py
+++ FIPS_data.py
@@ -16,12 +16,22 @@ FIPS_code.dropna(inplace=True)
 FIPS_code['county_name'] = FIPS_code['county_name'].map(lambda x: x.lstrip('+-').rstrip('County'))
 FIPS_code['County'] = FIPS_code['county_name']
 
+#create a FIPS dictionary and match to create FIPS column in Counties_States
+FIP_dict = dict(zip(FIPS_code.County, FIPS_code.county_FIPS))
+FIPS_dict = pd.Series(FIPS_code.County,index=FIPS_code.county_FIPS).to_dict()
+Counties_States['FIPS'] = Counties_States['County'].map(FIP_dict)
+
+#scratch to merge FIPS_code and Counties_States
 result_2 = pd.merge(Counties_States, FIPS_code, on='County')
 result_3 = pd.concat([Counties_States, FIPS_code], axis=0)
-
+df3 = pd.merge(Counties_States, FIPS_code, on=['County'],how='outer')
 result_4 = pd.merge(left=Counties_States, right=FIPS_code, left_on='County', right_on='County')
+
+#Boolean approach to matching
 import numpy as np
-Counties_States['County_FIPS'] = FIPS_code['county_name'] #add the Price2 column from df2 to df1
+Counties_States['County_FIPS'] = FIPS_code['county_name']
 merge(Counties_States, FIPS_code, by.x = 2, by.y = 0, all.x = TRUE)
 Counties_States['countiesMatch?'] = np.where(Counties_States['County'] == FIPS_code['County'], 'True', 'False')  #create new column in df1 to check if prices match
-#print (Counties_States)
+
+#export results
+result_3.to_csv(r'C:\Users\mcgr323\projects\tell\FIPS_match_2019.csv', index = False)
