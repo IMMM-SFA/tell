6924973056451ea0651bdb072b81ed3bf9e8f68d
crvernon <chrisrvernon@gmail.com>
2021-01-07 15:46:22 -0500

    update setup function with reqs and min version

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def get_requirements():
+    with open('requirements.txt') as f:
+        return f.read().split()
 
 setup(
     name='tell',
     version='0.1.0',
-    packages=['tell'],
+    packages=find_packages(),
     url='https://github.com/IMMM-SFA/tell.git',
     license='BSD2-Clause',
     author='Casey McGrath; Chris R. Vernon',
     author_email='casey.mcgrath@pnnl.gov; chris.vernon@pnnl.gov',
-    description='A model to predict total electricity loads'
+    description='A model to predict total electricity loads',
+    long_description=readme(),
+    python_requires='>=3.6.*, <4',
+    install_requires=get_requirements()
 )
