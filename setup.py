b2ef090752c2c04a69d7898a6770a15ab3c91886
crvernon <chrisrvernon@gmail.com>
2021-01-07 15:42:15 -0500

    add setup file for package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tell',
+    version='0.1.0',
+    packages=['tell'],
+    url='https://github.com/IMMM-SFA/tell.git',
+    license='BSD2-Clause',
+    author='Casey McGrath; Chris R. Vernon',
+    author_email='casey.mcgrath@pnnl.gov; chris.vernon@pnnl.gov',
+    description='A model to predict total electricity loads'
+)
