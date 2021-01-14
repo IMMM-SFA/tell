94771da27a1f8601124b7446212af2f19db29b3a
crvernon <chrisrvernon@gmail.com>
2021-01-14 15:31:15 -0500

    raise functions to package level

--- tell/__init__.py
+++ tell/__init__.py
@@ -1,16 +1,5 @@
 from tell.match import *
-
-__all__ = ['count_matches']
-__all__ = ['combine_elements']
-__all__ = ['keep_valid']
-__all__ = ['find_county']
-__all__ = ['get_max_count']
-__all__ = ['count_matches']
-__all__ = ['prepare_data']
-__all__ = ['filter_one']
-__all__ = ['filter_two']
-
 from tell.yearly_subset import *
 
-__all__ = ['prepare_data']
-__all__ = ['merge_and_subset']
+__all__ = ['count_matches', 'combine_elements', 'keep_valid', 'find_county', 'get_max_count', 'count_matches',
+           'prepare_data', 'filter_one', 'filter_two', 'prepare_data', 'merge_and_subset']
