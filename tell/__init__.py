9ecf3902cacc565e9a36429a932bb2427614d9de
crvernon <chrisrvernon@gmail.com>
2021-01-19 10:48:52 -0500

    fix naming functions; setup logger

--- tell/__init__.py
+++ tell/__init__.py
@@ -2,4 +2,4 @@ from tell.match import *
 from tell.yearly_subset import *
 
 __all__ = ['count_matches', 'combine_elements', 'keep_valid', 'find_county', 'get_max_count', 'count_matches',
-           'prepare_data', 'filter_one', 'filter_two', 'prepare_data', 'merge_and_subset']
+           'prepare_data', 'filter_one', 'filter_two', 'prepare_data', 'merge_and_subset', 'process_data']
--- tell/match.py
+++ tell/match.py
@@ -1,11 +1,11 @@
-"""match.py
-
-Functionality to join FIPS codes with utility zones.
-
-"""
+import os
+import logging
+import time
 
 import numpy as np
 import pandas as pd
+
+from tell.logger import Logger
 
 
 def count_matches(states_key, fips_key, ignore=['and', 'if', 'on', 'an', 'a', 'the']):
@@ -37,7 +37,7 @@ def count_matches(states_key, fips_key, ignore=['and', 'if', 'on', 'an', 'a', 't
     # if state names match proceed, else return None
     if states_state_name == fips_state_name:
 
-        # split out state abbreviation and space separators; only keep county info and ensure there
+        # split out state abbreviation and space seperators; only keep county info and ensure there
         #    are no underscores in the county name
         states_key_split = [ix[0] for ix in [i.split('-') for i in ' '.join(states_split[1:]).split(' ')]]
 
@@ -70,18 +70,7 @@ def count_matches(states_key, fips_key, ignore=['and', 'if', 'on', 'an', 'a', 't
 
 
 def combine_elements(part, part_list):
-    """Check for any "de" suffix and join to second position (e.g., "de witt" to "dewitt")
-
-        :param part:                 The suffix of interest to be searched for in the parts_list
-        :type part:                  str
-
-        :param part_list:           The str of interest separated into a list of parts
-        :type part_list:            list
-
-        :return:                    Joined part list with combined to suffix
-
-        """
-
+    # check for any "de" suffix and join to second position (e.g., "de witt" to "dewitt")
     if part in part_list:
         one_idx = part_list.index(part)
         combined = f"{part_list[one_idx]}{part_list[one_idx + 1]}"
@@ -93,15 +82,7 @@ def combine_elements(part, part_list):
 
 
 def keep_valid(x):
-    """Keep only dictionaries that have a count for a county name present.
-
-        :param x:                Dictionary with matches from filter_two
-
-        :type x:                  str
-
-        :return:                  Dictionary with count with county name
-
-        """
+    """Keep only dictionaries that have a count for a county name present."""
 
     d = {}
     for value in x:
@@ -114,14 +95,7 @@ def keep_valid(x):
 
 def find_county(d):
     """Add the FIPS key to the data frame where the optimal value with a count
-    of 1 has been identified.
-
-    :param d:              Dictionary with count with county name from keep_valid
-    :type d:               str
-
-    :return:               FIPS key combined to county dictionary with a count of 1 or more
-
-    """
+    of 1 has been identified."""
 
     if len(d) > 0:
 
@@ -145,14 +119,7 @@ def find_county(d):
 
 def get_max_count(d):
     """Generate a column that has the count of the optimal county name;
-    ideally this is 1, if a different number investigate futher
-
-    :param d:              Dictionary with FIPS key from find_county
-    :type d:               str
-
-    :return:               Dataframe of FIPS matches with column count of optimal county name
-
-    """
+    ideally this is 1, if a different number investigate futher"""
 
     if len(d) > 0:
 
@@ -172,25 +139,18 @@ def get_max_count(d):
         return None
 
 
-def prepare_data(fips_file, service_area_file):
+def prepare_data(fips_file, service_area_file, sales_ult_file, bal_auth_file):
     """Load and prepare data.  Reduce complexity by making state and county names lower case and splitting
     out commonly known trailing words that do not exist in both data sets.  Build key to join by
     where <state_abbrev>_<county_lower>.
-
-    :param fips_file:              FIPS csv input
-    :type fips_file:               str
-
-    :param service_area_file:     Service area csv input
-    :type service_area_file:      str
-
-    :return:                      df_fips: Dataframe of prepared and cleaned FIPS data
-    :return:                      df_states:: Dataframe of prepared and cleaned Service area data
 
     """
 
     # read in data
     df_fips = pd.read_excel(fips_file)
     df_states = pd.read_excel(service_area_file, sheet_name='Counties_States')
+    df_ult = pd.read_excel(sales_ult_file, sheet_name='States', skiprows=2)
+    df_ba = pd.read_excel(bal_auth_file)
 
     # strip county name from full reference and make lower case for known trailing words replace apostrophes
     df_fips['county_lower'] = df_fips['county_name'].apply(lambda x: x.lower().split(' county')[0])
@@ -208,25 +168,25 @@ def prepare_data(fips_file, service_area_file):
     df_fips['fips_key'] = df_fips['state_abbreviation'] + '_' + df_fips['county_lower']
     df_states['states_key'] = df_states['State'] + '_' + df_states['county_lower']
 
-    return df_fips, df_states
+    # filter df_ult and df_ba
+    df_ult = df_ult[["Utility Number", "Utility Name", "BA Code"]]
+    df_ba = df_ba[["BA Code", "BA ID", "Balancing Authority Name"]]
+
+    return df_fips, df_states, df_ult, df_ba
 
 
-def filter_one(df_fips, df_states):
-    """Join datasets together where possible based on common key.
+def filter_one(df_fips, df_states, df_ult, df_ba):
+    """Join datasets together where possible based on common key."""
 
-    :param fips_file:             Dataframe of prepared and cleaned FIPS data from prepare_data
-    :type fips_file:              str
+    # merge states and fips based on key
+    df_states_fips = pd.merge(left=df_states, right=df_fips, left_on='states_key', right_on='fips_key', how='left')
 
-    :param service_area_file:     Dataframe of prepared and cleaned Service area data from prepare_data
-    :type service_area_file:      str
+    # merge to states_fips merge to ba utility dataframe by ult number
+    df_states_fips['Utility Number'] = df_states_fips['Utility Number'].astype(float)
+    df_fips_ult = df_states_fips.merge(df_ult, on='Utility Number', how='left')
 
-    :return:                      Dataframe of valid data with a match between df_fips and df_states
-    :return:                      Dataframe of data without a match between df_fips and df_states
-    """
-
-
-    # merge based on key
-    df_valid = pd.merge(left=df_states, right=df_fips, left_on='states_key', right_on='fips_key', how='left')
+    # add the BA number and name by BA ulitity code
+    df_valid = df_fips_ult.merge(df_ba, on='BA Code', how='left')
 
     # filter out remaining rows that did not have a match
     df_nan = df_valid.loc[df_valid['county_lower_y'].isna()].copy()
@@ -235,27 +195,14 @@ def filter_one(df_fips, df_states):
     df_valid = df_valid.loc[~df_valid['county_lower_y'].isna()].copy()
 
     # drop unneeded columns
-    df_valid.drop(columns=['county_lower_y'], inplace=True)
+    df_valid.drop(columns=['county_lower_y', 'Utility Name_y'], inplace=True)
     df_nan.drop(columns=['county_lower_y'], inplace=True)
 
     return df_valid, df_nan
 
 
 def filter_two(df_fips, df_nan, df_valid):
-    """Match NaN records by cleaning up naming conventions based on the most suitable match.
-
-    :param df_fips:     Dataframe of prepared and cleaned FIPS data from prepare_data
-    :type df_fips:      str
-
-    :param df_nan:      Dataframe of data without a match between df_fips and df_states
-    :type df_nan:       str
-
-    :param df_valid:    Dataframe of valid data with a match between df_fips and df_states
-    :type df_valid:     str
-
-    :return:           df_valid: Dataframe of valid data with a match between df_fips and df_states
-    :return:           df_nan_bad: Dataframe of data without a match between df_fips and df_states
-    """
+    """Match NaN records by cleaning up naming conventions based on the most suitable match."""
 
     # get keys from states that are in teh FIPS code data frame that have NaN records
     nan_keys_fips = df_fips.loc[df_fips['state_abbreviation'].isin(df_nan['State'].unique())]['fips_key'].unique()
@@ -299,3 +246,95 @@ def filter_two(df_fips, df_nan, df_valid):
     df_valid = pd.concat([df_valid, mrx])
 
     return df_valid, df_nan_bad
+
+
+def data_format(df):
+    """Select for wanted columns and rename columns.
+
+    :param df:                  Data frame containing valid data.
+
+    :return:                    Data frame with relevant and renamed columns
+
+    """
+
+    # only keep columns that are needed
+    df = df[['Data Year', 'Utility Number', 'Utility Name_x', 'state_abbreviation', 'state_name',
+             'state_FIPS', 'county_name', 'county_FIPS', 'BA ID', 'BA Code',
+             'Balancing Authority Name']].copy()
+
+    # rename columns
+    df.rename(columns={"Date Year": "Year",
+                       "Utility Number": "Utility_Number",
+                       "Utility Name_x": "Utility_Name",
+                       "state_abbreviation": "State_Abbreviation",
+                       "state_name": "State_Name",
+                       "state_FIPS": "State_FIPS",
+                       "county_name": "County_Name",
+                       "county_FIPS": "County_FIPS",
+                       "BA ID": "BA_Number",
+                       "BA Code": "BA_Abbreviation",
+                       "Balancing Authority Name": "BA_Name"}, inplace=True)
+    return df
+
+
+def process_data(target_year, fips_file, service_area_file, sales_ult_file, bal_auth_file, output_dir):
+    """Workflow funciton to join files and clean up erroneous and missing data.  Suggest possible solutions from the
+    FIPS records for unmatched counties.
+
+    :param target_year:                         Year to process; four digit year (e.g., 1990)
+    :type target_year:                          int
+
+    #TODO:  Casey fill in other variable descriptions
+
+    :return:                                    Casey fill in
+
+    """
+
+    # initialize logger
+    logger = Logger(output_directory=output_dir)
+    logger.initialize_logger()
+
+    # report start time
+    logging.info("Start time:  {}".format(time.strftime("%Y-%m-%d %H:%M:%S")))
+
+    # prepare data
+    logging.info("Preparing data...")
+    df_fips, df_states, df_ult, df_ba = prepare_data(fips_file, service_area_file, sales_ult_file, bal_auth_file)
+
+    # apply filter one
+    logging.info("Applying filter one...")
+    df_valid, df_nan = filter_one(df_fips, df_states, df_ult, df_ba)
+
+    # apply filter two
+    logging.info("Applying filter two...")
+    df_valid, df_nan = filter_two(df_fips, df_nan, df_valid)
+
+    # format columns
+    logging.info("Formatting output data...")
+    df_valid = data_format(df_valid)
+
+    # get an array of counties that do not have a match
+    logging.info("Identifying unmatched data...")
+    unmatched_counties = df_nan['county_lower_x'].unique()
+
+    # report possible matches for the county
+    for i in unmatched_counties:
+        possible_matches = df_nan.loc[df_nan['county_lower_x'] == i]['matches'].values[0]
+
+        logging.info(f'Possible matches for unmatched county "{i}": from FIPS file: {possible_matches}')
+
+    # TODO:  is this needed?
+    # df_fips.loc[~df_fips['fips_key'].isin(df_valid['fips_key'])]
+
+    # write to CSV
+    output_file = os.path.join(output_dir, f'fips_service_match_{target_year}.csv')
+    logging.info(f"Writing output file to:  {output_file}")
+    df_valid.to_csv(output_file, sep=',', index=False)
+
+    # report close time
+    logging.info("End time:  {}".format(time.strftime("%Y-%m-%d %H:%M:%S")))
+
+    # close logger and clean up
+    logger.close_logger()
+
+    return df_valid
