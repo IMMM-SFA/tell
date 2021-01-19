a6758730c94d14afbaa3d5ab1910392613848a61
Casey McGrath <mcgrathc@hawaii.edu>
2021-01-18 16:54:05 -1000

    add column rename and BA ID

--- match_FIPS_code.py
+++ match_FIPS_code.py
@@ -134,7 +134,7 @@ def get_max_count(d):
         return None
 
 
-def prepare_data(fips_file, service_area_file):
+def prepare_data(fips_file, service_area_file, sales_ult_file, bal_auth_file):
     """Load and prepare data.  Reduce complexity by making state and county names lower case and splitting
     out commonly known trailing words that do not exist in both data sets.  Build key to join by
     where <state_abbrev>_<county_lower>.
@@ -144,6 +144,8 @@ def prepare_data(fips_file, service_area_file):
     # read in data
     df_fips = pd.read_excel(fips_file)
     df_states = pd.read_excel(service_area_file, sheet_name='Counties_States')
+    df_ult = pd.read_excel(sales_ult_file, sheet_name='States', skiprows=2)
+    df_ba = pd.read_excel(bal_auth_file)
 
     # strip county name from full reference and make lower case for known trailing words replace apostrophes
     df_fips['county_lower'] = df_fips['county_name'].apply(lambda x: x.lower().split(' county')[0])
@@ -161,14 +163,25 @@ def prepare_data(fips_file, service_area_file):
     df_fips['fips_key'] = df_fips['state_abbreviation'] + '_' + df_fips['county_lower']
     df_states['states_key'] = df_states['State'] + '_' + df_states['county_lower']
 
-    return df_fips, df_states
+    # filter df_ult and df_ba
+    df_ult = df_ult[["Utility Number", "Utility Name", "BA Code"]]
+    df_ba = df_ba[["BA Code", "BA ID", "Balancing Authority Name"]]
+
+    return df_fips, df_states, df_ult, df_ba
 
 
-def filter_one(df_fips, df_states):
+def filter_one(df_fips, df_states, df_ult, df_ba):
     """Join datasets together where possible based on common key."""
 
-    # merge based on key
-    df_valid = pd.merge(left=df_states, right=df_fips, left_on='states_key', right_on='fips_key', how='left')
+    # merge states and fips based on key
+    df_states_fips = pd.merge(left=df_states, right=df_fips, left_on='states_key', right_on='fips_key', how='left')
+
+    # merge to states_fips merge to ba utility dataframe by ult number
+    df_states_fips['Utility Number'] = df_states_fips['Utility Number'].astype(float)
+    df_fips_ult = df_states_fips.merge(df_ult, on='Utility Number', how='left')
+
+    # add the BA number and name by BA ulitity code
+    df_valid = df_fips_ult.merge(df_ba, on='BA Code', how='left')
 
     # filter out remaining rows that did not have a match
     df_nan = df_valid.loc[df_valid['county_lower_y'].isna()].copy()
@@ -177,7 +190,7 @@ def filter_one(df_fips, df_states):
     df_valid = df_valid.loc[~df_valid['county_lower_y'].isna()].copy()
 
     # drop unneeded columns
-    df_valid.drop(columns=['county_lower_y'], inplace=True)
+    df_valid.drop(columns=['county_lower_y', 'Utility Name_y'], inplace=True)
     df_nan.drop(columns=['county_lower_y'], inplace=True)
 
     return df_valid, df_nan
@@ -229,29 +242,47 @@ def filter_two(df_fips, df_nan, df_valid):
 
     return df_valid, df_nan_bad
 
+
+def data_format(df_valid):
+    """Select for wanted columns and rename columns."""
+
+    df_valid = df_valid.loc[['Data Year', 'Utility Number', 'Utility Name_x','state_abbreviation', 'state_name',
+                            'state_FIPS', 'county_name', 'county_FIPS', 'BA ID', 'BA Code', 'Balancing Authority Name']]
+
+    df_valid = df_valid.loc.rename(columns={"Date Year": "Year", "Utility Number": "Utility_Number",
+                                        "Utility Name_x": "Utility_Name", "state_abbreviation": "State_Abbreviation",
+                                        "state_name": "State_Name", "state_FIPS": "State_FIPS",
+                                        "county_name": "County_Name", "county_FIPS": "County_FIPS",
+                                        "BA ID": "BA_Number", "BA Code": "BA_Abbreviation",
+                                        "Balancing Authority Name": "BA_Name"})
+    return df_valid
+
+
 data_dir = 'C:\\Users\\mcgr323\\projects\\TELL\\inputs\\'
 data_dir_2 = 'C:\\Users\\mcgr323\\projects\\TELL\\inputs\\EIA_861\\Raw_Data\\2019\\'
 
 # paths to files
 fips_file = os.path.join(data_dir, 'state_and_county_fips_codes.xlsx')
 service_area_file = os.path.join(data_dir_2, 'Service_Territory_2019.xlsx')
+sales_ult_file = os.path.join(data_dir_2, 'Sales_Ult_Cust_2019.xlsx')
+bal_auth_file = os.path.join(data_dir_2, 'Balancing_Authority_2019.xlsx')
 
 # prepare data
-df_fips, df_states = prepare_data(fips_file, service_area_file)
+df_fips, df_states, df_ult, df_ba = prepare_data(fips_file, service_area_file, sales_ult_file, bal_auth_file)
 
 # apply filter one
-df_valid, df_nan = filter_one(df_fips, df_states)
+df_valid, df_nan = filter_one(df_fips, df_states, df_ult, df_ba)
 
 # apply filter two
 df_valid, df_nan = filter_two(df_fips, df_nan, df_valid)
 
-print(f"Number of records without a match:  {df_nan.shape[0]}")
+# format columns
+df_valid = data_format(df_valid)
 
 unmatched_counties = df_nan['county_lower_x'].unique()
 list(unmatched_counties)
 
 for i in unmatched_counties:
-
     possible_matches = df_nan.loc[df_nan['county_lower_x'] == i]['matches'].values[0]
 
     print(f"States file county: {i}")
