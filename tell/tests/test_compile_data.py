8cc0cb97c9d891eaca26bd88a30d2bccbe2c2ad0
Casey McGrath <mcgrathc@hawaii.edu>
2022-03-09 14:48:35 -1000

    update compile_data.py test

--- tell/tests/test_compile_data.py
+++ tell/tests/test_compile_data.py
@@ -3,6 +3,7 @@ import unittest
 
 from tell.data_process_compile_df import compile_data
 
+
 class TestCompileData(unittest.TestCase):
     """Tests for functionality within data_process_compile_df.py"""
 
@@ -12,6 +13,7 @@ class TestCompileData(unittest.TestCase):
         start_year = 2018
         end_year = 2019
         data_input_dir = os.path.join((os.path.dirname(os.getcwd())), r'tell_data')
+
         compile_data(start_year, end_year, data_input_dir)
 
 
