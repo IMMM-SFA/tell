23dc2bc1ce9297af5543be616e03722b4f436865
Chris Vernon <chrisrvernon@gmail.com>
2022-03-23 15:12:48 -0400

    add parallel functionality to mlp

--- tell/mlp.py
+++ tell/mlp.py
@@ -1,18 +1,11 @@
-import os
-import glob
-import warnings
 from typing import Union
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from pandas import DataFrame
-from scipy.stats import pearsonr
+from joblib import Parallel, delayed
 from sklearn.linear_model import LinearRegression as LR
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_percentage_error
 from sklearn.neural_network import MLPRegressor as MLP
-from sklearn.model_selection import GridSearchCV
 
 from tell.mlp_prepare_data import Dataset, DefaultSettings
 
@@ -69,21 +62,39 @@ def scale_features(x_train: np.ndarray,
     return dict_out
 
 
-def denormlaize(normalized_dict: dict,
-                y_predicted_normalized: np.ndarray) -> np.ndarray:
+def denormlaize(region: str,
+                normalized_dict: dict,
+                y_predicted_normalized: np.ndarray,
+                datetime_arr: np.ndarray) -> pd.DataFrame:
     """Function to denormlaize the predictions of the model.
 
-    :param normalized_dict:                         Dictionary output from normalization function.
-    :type normalized_dict:                          dict
+    :param region:                              Indicating region / balancing authority we want to train and test on.
+                                                Must match with string in CSV files.
+    :type region:                               str
 
-    :param y_predicted_normalized:                  Normalized predictions over the test set.
-    :type y_predicted_normalized:                   np.ndarray
+    :param normalized_dict:                     Dictionary output from normalization function.
+    :type normalized_dict:                      dict
 
-    :return:                                        Denormalized predictions
+    :param y_predicted_normalized:              Normalized predictions over the test set.
+    :type y_predicted_normalized:               np.ndarray
+
+    :param datetime_arr:                        Array of datetimes corresponding to the predictions.
+    :type datetime_arr:                         np.ndarray
+
+    :return:                                    Denormalized predictions
 
     """
 
-    return y_predicted_normalized * normalized_dict["sigma_y_train"] + normalized_dict["mu_y_train"]
+    # denormalize predicted Y
+    y_p = y_predicted_normalized * normalized_dict["sigma_y_train"] + normalized_dict["mu_y_train"]
+
+    # create data frame with datetime attached
+    df = pd.DataFrame({"Datetime": datetime_arr, "Predictions": y_p})
+
+    # add in region field
+    df["region"] = region
+
+    return df
 
 
 def run_linear_model(x_train: np.ndarray,
@@ -382,13 +393,131 @@ def predict(region: str,
                                            x_linear_test=x_linear_test)
 
     # denormalize predicted data
-    y_predicted = denormlaize(normalized_dict=normalized_dict,
-                              y_predicted_normalized=y_predicted_normalized)
+    prediction_df = denormlaize(region=region,
+                                normalized_dict=normalized_dict,
+                                y_predicted_normalized=y_predicted_normalized,
+                                datetime_arr=data_mlp.df_test["Datetime"].values)
 
     # generate validation stats
     validation_df = validation(region=region,
-                               y_predicted=y_predicted,
+                               y_predicted=prediction_df["Predictions"].values,
                                y_comparison=data_mlp.y_comp,
                                nodata_value=settings.nodata_value)
 
-    return y_predicted, validation_df
+    return prediction_df, validation_df
+
+
+def predict_batch(target_region_list: list,
+                  data_dir: str,
+                  n_jobs: int = -1,
+                  **kwargs):
+    """Generate predictions for MLP model for a target region from an input CSV file.
+
+    :param target_region_list:          List of names indicating region / balancing authority we want to train and test
+                                        on. Must match with string in CSV files.
+    :type target_region_list:           list
+
+    :param data_dir:                    Full path to the directory that houses the input CSV files.
+    :type data_dir:                     str
+
+    :param n_jobs:                      The maximum number of concurrently running jobs, such as the number of Python
+                                        worker processes when backend=”multiprocessing” or the size of the thread-pool
+                                        when backend=”threading”. If -1 all CPUs are used. If 1 is given, no parallel
+                                        computing code is used at all, which is useful for debugging. For n_jobs
+                                        below -1, (n_cpus + 1 + n_jobs) are used. Thus for n_jobs = -2, all CPUs
+                                        but one are used. None is a marker for ‘unset’ that will be interpreted as
+                                        n_jobs=1 (sequential execution) unless the call is performed under a
+                                        parallel_backend context manager that sets another value for n_jobs.
+    :type n_jobs:                       int
+
+    :param mlp_hidden_layer_sizes:      The ith element represents the number of neurons in the ith hidden layer.
+    :type mlp_hidden_layer_sizes:       Optional[int]
+
+    :param mlp_max_iter:                Maximum number of iterations. The solver iterates until convergence
+                                        (determined by ‘tol’) or this number of iterations. For stochastic solvers
+                                        (‘sgd’, ‘adam’), note that this determines the number of epochs (how many
+                                        times each data point will be used), not the number of gradient steps.
+    :type mlp_max_iter:                 Optional[int]
+
+    :param mlp_validation_fraction:     The proportion of training data to set aside as validation set for early
+                                        stopping. Must be between 0 and 1.
+    :type mlp_validation_fraction:      Optional[float]
+
+    :param mlp_linear_adjustment:       True if you want to correct the MLP model using a linear model.
+    :type mlp_linear_adjustment:        Optional[bool]
+
+    :param apply_sine_function:         True if setting up data for a linear model that will be run and will cause
+                                        the application of the sine function for hour and month fields if they
+                                        are present in the data.
+    :type apply_sine_function:          Optional[bool]
+
+    :param data_column_rename_dict:     Dictionary for the field names present in the input CSV file (keys) to what the
+                                        code expects them to be (values).
+    :type data_column_rename_dict:      Optional[dict[str]]
+
+    :param expected_datetime_columns:   Expected names of the date time columns in the input CSV file.
+    :type expected_datetime_columns:    Optional[list[str]]
+
+    :param hour_field_name:             Field name of the hour field in the input CSV file.
+    :type hour_field_name:              Optional[str]
+
+    :param month_field_name:            Field name of the month field in the input CSV file.
+    :type month_field_name:             Optional[str]
+
+    :param x_variables:                 Target variable list.
+    :type x_variables:                  Optional[list[str]]
+
+    :param add_dayofweek_xvars:         True if the user wishes to add weekday and holiday targets to the x variables.
+    :type add_dayofweek_xvars:          Optional[bool]
+
+    :param y_variables:                 Feature variable list.
+    :type y_variables:                  Optional[list[str]]
+
+    :param day_list:                    List of day abbreviations and their order.
+    :type day_list:                     Optional[list[str]]
+
+    :param start_time:                  Timestamp showing the datetime of for the run to start
+                                        (e.g., 2016-01-01 00:00:00).
+    :type start_time:                   Optional[str]
+
+    :param end_time:                    Timestamp showing the datetime of for the run to end
+                                        (e.g., 2019-12-31 23:00:00).
+    :type end_time:                     Optional[str]
+
+    :param split_datetime:              Timestamp showing the datetime to split the train and test data by
+                                        (e.g., 2018-12-31 23:00:00).
+    :type split_datetime:               Optional[str]
+
+    :param nodata_value:                No data value in the input CSV file.
+    :type nodata_value:                 Optional[int]
+
+    :param seed_value:                  Seed value to reproduce randomization.
+    :type seed_value:                   Optional[int]
+
+    :param x_variables_linear:          Target variable list for the linear model.
+    :type x_variables_linear:           Optional[list[str]]
+
+    :param y_variables_linear:          Feature variable list for the linear model.
+    :type y_variables_linear:           Optional[list[str]]
+
+    """
+
+    # run all regions in target list in parallel
+    results = Parallel(n_jobs=n_jobs)(delayed(predict)(region=region,
+                                                       data_dir=data_dir,
+                                                       **kwargs) for region in target_region_list)
+
+    # aggregate outputs
+    for index, i in enumerate(results):
+
+        if index == 0:
+            prediction_df = i[0]
+            validation_df = i[1]
+        else:
+            prediction_df = pd.concat([prediction_df, i[0]])
+            validation_df = pd.concat([validation_df, i[1]])
+
+    return prediction_df, validation_df
+
+
+
