bdbb106fbd2b4aa76f11adc3081170ca5d36f2bc
Casey McGrath <mcgrathc@hawaii.edu>
2022-03-23 14:22:46 -1000

    add to compile data test

--- tell/tests/test_data_process_compile.py
+++ tell/tests/test_data_process_compile.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import pandas as pd
 
 from tell.tell.data_process_compile import compile_data
 
@@ -10,11 +11,18 @@ class TestCompileData(unittest.TestCase):
     def test_compile_data(self):
         """Test to ensure high level functionality of data_process_compile_df.py()"""
 
-        start_year = 2018
+        start_year = 2015
         end_year = 2019
         data_input_dir = os.path.join((os.path.dirname(os.getcwd())), r'tell_data')
 
         compile_data(start_year, end_year, data_input_dir)
+
+        AEC_file = os.path.join((os.path.dirname(os.getcwd())), r'tell_data', r'outputs', r'compiled_historical_data',
+                                "AEC_historical_data.csv")
+
+        AEC_df = pd.read_csv(AEC_file)
+
+
 
 
 if __name__ == '__main__':
