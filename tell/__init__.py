079206dfbb764039d7c21b517f256dd69c910f61
Chris Vernon <chrisrvernon@gmail.com>
2022-05-09 18:16:08 -0400

    remove old models and add imports

--- tell/__init__.py
+++ tell/__init__.py
@@ -12,6 +12,7 @@ from .install_raw_data import install_tell_raw_data
 from .install_forcing_data import install_sample_forcing_data
 
 # ml modeling
+from .mlp_prepare_data import *
 from .mlp_train import train, train_batch
 from .mlp_utils import *
 from .mlp_predict import predict, predict_batch
